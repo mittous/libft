397b1566bd76f43bf7a21367f6634c8b050bb03b
Ismail Mittous <imittous@student.1337.ma>
2022-10-04 17:16:26 +0100

    Making my one 'C' library

--- ft_tolower.c
+++ ft_tolower.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: imittous <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: imittous <imittous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 16:27:27 by imittous          #+#    #+#             */
-/*   Updated: 2021/11/11 16:28:59 by imittous         ###   ########.fr       */
+/*   Updated: 2022/10/04 15:41:54 by imittous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,8 @@ int	ft_tolower(int c)
 		c += 32;
 	return (c);
 }
+
+	// The ft_tolower() function converts a upper-case letter
+	// to the corresponding lower-case letter.
+	// The ft_tolower() function return if the argument is a upper-case letter, the corresponding 
+	// lower-case letter if there is one; otherwise, the argument is returned unchanged.
--- ft_toupper.c
+++ ft_toupper.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: imittous <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: imittous <imittous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 16:29:17 by imittous          #+#    #+#             */
-/*   Updated: 2021/11/11 17:49:52 by imittous         ###   ########.fr       */
+/*   Updated: 2022/10/04 15:42:01 by imittous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,8 @@ int	ft_toupper(int c)
 		c -= 32;
 	return (c);
 }
+
+	// The ft_toupper() function converts a lower-case letter
+	// to the corresponding upper-case letter.
+	// The ft_toupper() function return if the argument is a lower-case letter, the corresponding 
+	// upper-case letter if there is one; otherwise, the argument is returned unchanged.
